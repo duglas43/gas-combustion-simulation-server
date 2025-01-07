004317d6c58247be0d38e74bb675e31449e92ecb
Evgenii Malkov <zenamalkov1@gmail.com>
2025-01-07 06:20:17 +0300

    feat: add economizer characteristic creation through application

--- src/calculations/calculations.service.ts
+++ src/calculations/calculations.service.ts
@@ -64,7 +64,9 @@ export class CalculationsService {
     await this.furnaceHeatBalanceRepository.delete({});
     await this.heatBalanceRepository.delete({});
     await this.temperatureCharacteristicRepository.delete({});
+    await this.economizerCharacteristicRepository.delete({});
 
+    await this.createEconomizerCharacteristic();
     await this.createBoilerCharacteristic(dto);
     await this.createFuelComposition(dto);
     await this.createFurnaceCharacteristic(dto);
@@ -80,6 +82,33 @@ export class CalculationsService {
     await this.createSecondConvectivePackageHeatBalance();
     await this.createEconomizerHeatBalance();
     return 'Ok';
+  }
+
+  async createEconomizerCharacteristic() {
+    const economizerCharacteristic =
+      this.economizerCharacteristicRepository.create({
+        outerCasingTubeDiameter: 76,
+        finThickness: 5,
+        finPitch: 25,
+        finSize: 146,
+        tubePitchInRow: 150,
+        rowPitch: 150,
+        tubesPerRow: 7,
+        numberOfRows: 16,
+        averageTubeLength: 3,
+        heatTransferSurfaceAreaPerTube: 4.49,
+        finHeight: 35,
+        relativeTubePitchInRow: 1.974,
+        relativeRowPitch: 1.974,
+        totalEconomizerTubes: 112,
+        numberOfColumns: 2,
+        totalHeatTransferSurfaceArea: 502.9,
+        channelCrossSectionArea: 1.26,
+        equivalentChannelDiameter: 0.0304,
+      });
+    return await this.economizerCharacteristicRepository.save(
+      economizerCharacteristic,
+    );
   }
 
   async createBoilerCharacteristic(dto: CreateCalculationDto) {
